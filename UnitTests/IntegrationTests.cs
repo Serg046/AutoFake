4f36c49f5f3b7f1f0edc5a804ffae3b08b28b460
Aseev Sergey <serg046@outlook.com>
2016-08-09 04:07:49 +0300

    Refactored IntegrationTests.cs

--- UnitTests/IntegrationTests.cs
+++ UnitTests/IntegrationTests.cs
@@ -1,52 +1,54 @@
 ﻿using AutoFake;
 using System;
-using System.Linq;
 using Xunit;
 
 namespace UnitTests
 {
     public class IntegrationTests
     {
+        private readonly DateTime _currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local);
+
+        [Fact]
+        public void InstancePropertyMockWorksFine()
+        {
+            var calendarFake = Fake.For<Calendar>(Calendar.GetTimeZone())
+                .Setup((Calendar c) => c.UtcDate)
+                .ReachableWith(c => c.GetUtcDate())
+                .Returns(_currentDate);
+
+            Assert.Equal(_currentDate, calendarFake.Execute(c => c.GetUtcDate()));
+        }
+
+        [Fact]
+        public void ExternalPropertyMockWorksFine()
+        {
+            var calendarFake = Fake.For<Calendar>(Calendar.GetTimeZone())
+                .Setup((DateCalculator dc) => dc.UtcDate)
+                .ReachableWith(c => c.CalculateUtcDate())
+                .Returns(_currentDate);
+
+            Assert.Equal(_currentDate, calendarFake.Execute(c => c.CalculateUtcDate()));
+        }
+
         [Fact]
         public void StaticPropertyMockWorksFine()
         {
-            var currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local); 
-
-            var timeZone = TimeZoneInfo.GetSystemTimeZones().Single(t => t.Id == "UTC");
-            var calendarFake = Fake.For<Calendar>(timeZone)
+            var calendarFake = Fake.For<Calendar>(Calendar.GetTimeZone())
                 .Setup(() => DateTime.Now)
                 .ReachableWith(c => c.GetDate())
-                .Returns(currentDate);
+                .Returns(_currentDate);
 
             var expectedDate = new DateTime(2016, 8, 8, 12, 00, 00);
             Assert.Equal(expectedDate, calendarFake.Execute(c => c.GetDate()));
         }
 
         [Fact]
-        public void InnerCallOfMockedMethodWorksFine()
-        {
-            var currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local);
-
-            var timeZone = TimeZoneInfo.GetSystemTimeZones().Single(t => t.Id == "UTC");
-            var calendarFake = Fake.For<Calendar>(timeZone)
-                .Setup(() => DateTime.Now)
-                .ReachableWith(c => c.GetDateWithInnerCall())
-                .Returns(currentDate);
-
-            var expectedDate = new DateTime(2016, 8, 8, 12, 00, 00);
-            Assert.Equal(expectedDate, calendarFake.Execute(c => c.GetDateWithInnerCall()));
-        }
-
-        [Fact]
         public void InstanceMethodMockWorksFine()
         {
-            var currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local);
-
-            var timeZone = TimeZoneInfo.GetSystemTimeZones().Single(t => t.Id == "UTC");
-            var calendarFake = Fake.For<Calendar>(timeZone)
+            var calendarFake = Fake.For<Calendar>(Calendar.GetTimeZone())
                 .Setup((Calendar c) => c.GetCurrentDate())
                 .ReachableWith(c => c.GetDateWithInnerCall())
-                .Returns(currentDate);
+                .Returns(_currentDate);
 
             var expectedDate = new DateTime(2016, 8, 8, 12, 00, 00);
             Assert.Equal(expectedDate, calendarFake.Execute(c => c.GetDateWithInnerCall()));
@@ -55,29 +57,34 @@ namespace UnitTests
         [Fact]
         public void ExternalMethodMockWorksFine()
         {
-            var currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local);
             var offset = 1;
-
-            var timeZone = TimeZoneInfo.GetSystemTimeZones().Single(t => t.Id == "UTC");
-            var calendarFake = Fake.For<Calendar>(timeZone)
-                .Setup((DateCalculator dc) => dc.AddHours(currentDate, offset))
+            var calendarFake = Fake.For<Calendar>(Calendar.GetTimeZone())
+                .Setup((DateCalculator dc) => dc.AddHours(_currentDate, offset))
                 .ReachableWith(c => c.GetDateWithOffset(offset))
-                .Returns(currentDate);
-            
-            Assert.Equal(currentDate, calendarFake.Execute(c => c.GetDateWithOffset(offset)));
+                .Returns(_currentDate);
+
+            Assert.Equal(_currentDate, calendarFake.Execute(c => c.GetDateWithOffset(offset)));
+        }
+
+        [Fact]
+        public void StaticMethodMockWorksFine()
+        {
+            var calendarFake = Fake.For<Calendar>(Calendar.GetTimeZone())
+                .Setup(() => Calendar.GetTimeZone())
+                .ReachableWith(c => c.GetTimeZoneInfo())
+                .Returns(null);
+
+            Assert.Null(calendarFake.Execute(c => c.GetTimeZoneInfo()));
         }
 
         [Fact]
         public void MultipleReachableWithWorksFine()
         {
-            var currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local);
-
-            var timeZone = TimeZoneInfo.GetSystemTimeZones().Single(t => t.Id == "UTC");
-            var calendarFake = Fake.For<Calendar>(timeZone)
+            var calendarFake = Fake.For<Calendar>(Calendar.GetTimeZone())
                 .Setup(() => DateTime.Now)
                 .ReachableWith(c => c.GetDate())
                 .ReachableWith(c => c.GetDateWithInnerCall())
-                .Returns(currentDate);
+                .Returns(_currentDate);
 
             var expectedDate = new DateTime(2016, 8, 8, 12, 00, 00);
             Assert.Equal(expectedDate, calendarFake.Execute(c => c.GetDate()));
@@ -85,45 +92,15 @@ namespace UnitTests
         }
 
         [Fact]
-        public void InstancePropertyMockWorksFine()
+        public void InnerCallOfMockedMethodWorksFine()
         {
-            var currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local);
+            var calendarFake = Fake.For<Calendar>(Calendar.GetTimeZone())
+                .Setup(() => DateTime.Now)
+                .ReachableWith(c => c.GetDateWithInnerCall())
+                .Returns(_currentDate);
 
-            var timeZone = TimeZoneInfo.GetSystemTimeZones().Single(t => t.Id == "UTC");
-            var calendarFake = Fake.For<Calendar>(timeZone)
-                .Setup((Calendar c) => c.UtcDate)
-                .ReachableWith(c => c.GetUtcDate())
-                .Returns(currentDate);
-
-            Assert.Equal(currentDate, calendarFake.Execute(c => c.GetUtcDate()));
-        }
-
-        [Fact]
-        public void ExternalPropertyMockWorksFine()
-        {
-            var currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local);
-
-            var timeZone = TimeZoneInfo.GetSystemTimeZones().Single(t => t.Id == "UTC");
-            var calendarFake = Fake.For<Calendar>(timeZone)
-                .Setup((DateCalculator dc) => dc.UtcDate)
-                .ReachableWith(c => c.CalculateUtcDate())
-                .Returns(currentDate);
-
-            Assert.Equal(currentDate, calendarFake.Execute(c => c.CalculateUtcDate()));
-        }
-
-        [Fact]
-        public void StaticMethodMockWorksFine()
-        {
-            var currentDate = new DateTime(2016, 8, 8, 15, 00, 00, DateTimeKind.Local);
-
-            var timeZone = TimeZoneInfo.GetSystemTimeZones().Single(t => t.Id == "UTC");
-            var calendarFake = Fake.For<Calendar>(timeZone)
-                .Setup(() => Calendar.GetTimeZone())
-                .ReachableWith(c => c.GetTimeZoneInfo())
-                .Returns(null);
-
-            Assert.Null(calendarFake.Execute(c => c.GetTimeZoneInfo()));
+            var expectedDate = new DateTime(2016, 8, 8, 12, 00, 00);
+            Assert.Equal(expectedDate, calendarFake.Execute(c => c.GetDateWithInnerCall()));
         }
     }
 }
