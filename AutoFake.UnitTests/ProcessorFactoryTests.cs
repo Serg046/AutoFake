a5c0beea012bddbbdbef5f73c93e701373513d6d
Serg046 <serg046@outlook.com>
2020-05-21 00:48:38 +0300

    Add a few tests

---
+++ AutoFake.UnitTests/ProcessorFactoryTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Moq;
+using Xunit;
+
+namespace AutoFake.UnitTests
+{
+    public class ProcessorFactoryTests
+    {
+        [Theory, AutoMoqData]
+        internal void TypeInfo_ValidData_Success(ITypeInfo typeInfo)
+        {
+            var factory = new ProcessorFactory(typeInfo);
+
+            Assert.Equal(typeInfo, factory.TypeInfo);
+        }
+
+        [Theory, AutoMoqData]
+        internal void CreatePrePostProcessor_TypeInfo_Injected(
+            string name,
+            [Frozen]Mock<ITypeInfo> type,
+            ProcessorFactory factory)
+        {
+            var proc = factory.CreatePrePostProcessor();
+            proc.GenerateSetupBodyField(name);
+
+            type.Verify(t => t.AddField(It.Is<FieldDefinition>(f => f.Name == name)));
+        }
+
+        [Theory, AutoMoqData]
+        internal void CreateProcessor_TypeInfo_Injected(
+            string name,
+            [Frozen]Mock<ITypeInfo> type, Instruction cmd,
+            IEmitter emitter, 
+            ProcessorFactory factory)
+        {
+            var proc = factory.CreateProcessor(emitter, cmd);
+
+            try
+            {
+                proc.InjectCallback(null, false);
+            }
+            catch { }
+
+            type.Verify(t => t.Module);
+        }
+    }
+}
---
+++ AutoFake.UnitTests/Setup/MockCollectionTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoFake.Setup;
+using AutoFake.Setup.Mocks;
+using Moq;
+using Xunit;
+
+namespace AutoFake.UnitTests.Setup
+{
+    public class MockCollectionTests
+    {
+        [Theory, AutoMoqData]
+        internal void Add_ValidInput_Added(
+            Mock<MethodBase> method, ICollection<IMock> mocks,
+            MockCollection sut)
+        {
+            sut.Add(method.Object, mocks);
+
+            var item = sut.Single();
+            Assert.Equal(method.Object, item.Method);
+            Assert.Equal(mocks, item.Mocks);
+        }
+    }
+}
