5b72053a805958bdd74d316eca48a3d2c1676536
Serg046 <serg046@outlook.com>
2020-06-06 19:10:37 +0300

    Get rid of nuspec

--- AutoFake/Properties/AssemblyInfo.cs
+++ AutoFake/Properties/AssemblyInfo.cs
@@ -1,19 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("AutoFake")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoFake")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("8a751994-4ae2-40b9-8ed0-2b4875f3cafc")]
-[assembly: AssemblyVersion("0.2.3.0")]
-[assembly: AssemblyFileVersion("0.2.3.0")]
 [assembly: InternalsVisibleTo("AutoFake.UnitTests")]
 [assembly: InternalsVisibleTo("AutoFake.IntegrationTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
