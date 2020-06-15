e3a177f3c3aaae09211bf7e7b455ab36e9e68ac1
Serg046 <serg046@outlook.com>
2020-06-15 23:04:33 +0300

    Add ExternalAssemblySutTests

---
+++ AutoFake.IntegrationTests/ExternalAssemblySutTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AutoFake.IntegrationTests.Sut;
+using Xunit;
+
+namespace AutoFake.IntegrationTests
+{
+    public class ExternalAssemblySutTests
+    {
+        [Fact]
+        public void When_SimpleLambda_Should_Pass()
+        {
+            var fake = new Fake<SystemUnderTest>();
+
+            fake.Execute(f => f.SimpleMethod());
+        }
+
+        [Fact]
+        public void When_InternalSut_Should_Pass()
+        {
+            var fake = new Fake<SystemUnderTest>();
+
+            fake.Execute(f => f.InternalMethod());
+        }
+
+        [Fact]
+        public void When_ExpectedCallsFunc_ShouldPass()
+        {
+            var fake = new Fake<SystemUnderTest>();
+
+            var sut = fake.Rewrite(f => f.GetCurrentDate());
+            sut.Replace(() => DateTime.Now)
+                .ExpectedCalls(b => b > 0)
+                .Return(DateTime.MaxValue);
+
+            Assert.Equal(DateTime.MaxValue, sut.Execute());
+        }
+    }
+}
