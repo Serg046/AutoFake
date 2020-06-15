ce213684f2136fd7a077ec0e870107287900b17d
Serg046 <serg046@outlook.com>
2020-06-15 23:04:33 +0300

    Add ExternalAssemblySutTests

---
+++ AutoFake.IntegrationTests.Sut/SystemUnderTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AutoFake.IntegrationTests.Sut
+{
+    public class SystemUnderTest
+    {
+        public void SimpleMethod()
+        {
+        }
+
+        internal void InternalMethod()
+        {
+        }
+
+        public DateTime GetCurrentDate() => DateTime.Now;
+    }
+}
