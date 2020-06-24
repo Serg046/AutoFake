42b8e2b49ec62c5f70491a4a9ee3f36e0b516bd9
Serg046 <serg046@outlook.com>
2020-06-24 22:54:28 +0300

    Move Calendar model to a separate assembly

---
+++ AutoFake.IntegrationTests.Sut/Calendar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AutoFake.IntegrationTests.Sut
+{
+    public class Calendar
+    {
+        public static DateTime Yesterday => DateTime.Now.AddDays(-1);
+        internal Task<DateTime> AddSomeMinutesAsync(DateTime date) => Task.Run(() => AddSomeMinutes(date));
+        public static DateTime AddSomeMinutes(DateTime date) => date.AddMinutes(new Random().Next(1, 10));
+    }
+}
