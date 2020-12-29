77d785a9c3799b7e97b29f6ecacc5c164593e411
Serg046 <serg046@outlook.com>
2020-12-30 01:48:30 +0300

    Rename integration tests to functional tests

---
+++ Tests/AutoFake.FunctionalTests/InstanceTests/FieldMockTests.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using Xunit;
+
+namespace AutoFake.IntegrationTests.InstanceTests
+{
+	public class FieldMockTests
+	{
+		[Fact]
+		public void OwnInstanceTest()
+		{
+			var fake = new Fake<TestClass>();
+        
+			var sut = fake.Rewrite(f => f.GetDynamicValue());
+			sut.Replace(t => t.DynamicValue).Return(7);
+
+			Assert.Equal(7, sut.Execute());
+		}
+
+		[Fact]
+		public void ExternalInstanceTest()
+		{
+			var fake = new Fake<TestClass>();
+
+			var sut = fake.Rewrite(f => f.GetHelperDynamicValue());
+			sut.Replace((HelperClass h) => h.DynamicValue).Return(7);
+
+			Assert.Equal(7, sut.Execute());
+		}
+
+		[Fact]
+		public void OwnStaticTest()
+		{
+			var fake = new Fake<TestClass>();
+
+			var sut = fake.Rewrite(f => f.GetDynamicStaticValue());
+			sut.Replace(() => TestClass.DynamicStaticValue).Return(7);
+
+			Assert.Equal(7, sut.Execute());
+		}
+
+		[Fact]
+		public void ExternalStaticTest()
+		{
+			var fake = new Fake<TestClass>();
+
+			var sut = fake.Rewrite(f => f.GetHelperDynamicStaticValue());
+			sut.Replace(() => HelperClass.DynamicStaticValue).Return(7);
+
+			Assert.Equal(7, sut.Execute());
+		}
+
+		[Fact]
+		public void FrameworkTest()
+		{
+			var fake = new Fake<TestClass>();
+
+			const float value = 78;
+			var sut = fake.Rewrite(f => f.GetFrameworkValue());
+			sut.Replace((Quaternion quaternion) => quaternion.X).Return(value);
+
+			Assert.Equal(value, sut.Execute());
+		}
+
+		[Fact]
+		public void FrameworkStaticTest()
+		{
+			var fake = new Fake<TestClass>();
+
+			var sr = new StringReader(string.Empty);
+			var sut = fake.Rewrite(f => f.GetFrameworkStaticValue());
+			sut.Replace(() => TextReader.Null).Return(sr);
+
+			var actual = sut.Execute();
+			Assert.Equal(sr, actual);
+			Assert.NotEqual(TextReader.Null, actual);
+		}
+
+		[Fact]
+		public void StructFieldByAddress()
+		{
+			var fake = new Fake<TestClass>();
+
+			var data = new HelperStruct {Value = 5};
+			var sut = fake.Rewrite(f => f.GetStructValueByAddress());
+			sut.Replace(f => f.StructValue).Return(data);
+
+			Assert.Equal(data.Value, sut.Execute());
+		}
+
+		[Fact]
+		public void StaticStructFieldByAddress()
+		{
+			var fake = new Fake<TestClass>();
+
+			var data = new HelperStruct {Value = 5};
+			var sut = fake.Rewrite(f => f.GetStaticStructValueByAddress());
+			sut.Replace(() => TestClass.StaticStructValue).Return(data);
+
+			Assert.Equal(data.Value, sut.Execute());
+		}
+
+		[Fact]
+		public void GenericTest()
+		{
+			var fake = new Fake<GenericTestClass<int>>();
+
+			var sut = fake.Rewrite(f => f.GetValue(0, "0"));
+			sut.Replace(s => s.Pair).Return(new KeyValuePair<int, string>(1, "1"));
+
+			var actual = sut.Execute();
+			Assert.Equal(1, actual.Key);
+			Assert.Equal("1", actual.Value);
+		}
+
+		private class GenericTestClass<T>
+		{
+			public KeyValuePair<T, string> Pair = new KeyValuePair<T, string>(default, "test");
+			public KeyValuePair<T, string> GetValue(T x, string y) => Pair;
+		}
+
+#pragma warning disable 0649
+		private class TestClass
+		{
+			public int DynamicValue = 5;
+			public static int DynamicStaticValue = 5;
+			public HelperStruct StructValue;
+			public static HelperStruct StaticStructValue;
+
+			public int GetDynamicValue()
+			{
+				Debug.WriteLine("Started");
+				var value = DynamicValue;
+				Debug.WriteLine("Finished");
+				return value;
+			}
+
+			public int GetHelperDynamicValue()
+			{
+				Debug.WriteLine("Started");
+				var helper = new HelperClass();
+				var value = helper.DynamicValue;
+				Debug.WriteLine("Finished");
+				return value;
+			}
+
+			public int GetDynamicStaticValue()
+			{
+				Debug.WriteLine("Started");
+				var value = DynamicStaticValue;
+				Debug.WriteLine("Finished");
+				return value;
+			}
+
+			public int GetHelperDynamicStaticValue()
+			{
+				Debug.WriteLine("Started");
+				var value = HelperClass.DynamicStaticValue;
+				Debug.WriteLine("Finished");
+				return value;
+			}
+
+			public float GetFrameworkValue()
+			{
+				Debug.WriteLine("Started");
+				var quaternion = new Quaternion();
+				var value = quaternion.X;
+				Debug.WriteLine("Finished");
+				return value;
+			}
+			public TextReader GetFrameworkStaticValue()
+			{
+				Debug.WriteLine("Started");
+				var value = TextReader.Null;
+				Debug.WriteLine("Finished");
+				return value;
+			}
+
+			public int GetStructValueByAddress()
+			{
+				Debug.WriteLine("Started");
+				var value = StructValue.Value;
+				Debug.WriteLine("Finished");
+				return value;
+			}
+
+			public int GetStaticStructValueByAddress()
+			{
+				Debug.WriteLine("Started");
+				var value = StaticStructValue.Value;
+				Debug.WriteLine("Finished");
+				return value;
+			}
+		}
+
+		private class HelperClass
+		{
+			public int DynamicValue = 5;
+			public static int DynamicStaticValue = 5;
+		}
+
+		public struct HelperStruct
+		{
+			public int Value;
+		}
+	}
+}
