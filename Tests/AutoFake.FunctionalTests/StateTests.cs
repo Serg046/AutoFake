0b2a9fe0d96a446c0f27b9b105da3264f506e037
Serg046 <serg046@outlook.com>
2022-08-21 16:35:26 +0300

    Add When_invalid_attribute_Should_skip test

--- Tests/AutoFake.FunctionalTests/StateTests.cs
+++ Tests/AutoFake.FunctionalTests/StateTests.cs
@@ -101,6 +101,39 @@ namespace AutoFake.FunctionalTests
 			act.Should().Throw<InvalidOperationException>().WithMessage("Cannot find a type");
 		}
 
+		[Fact]
+		public void When_valid_state_machine_attribute_Should_skip()
+		{
+			var fake = new Fake<TestClass>();
+
+			var sut = fake.Rewrite(f => f.GetDateWithValidMachine());
+			sut.Replace((StateMachine m) => m.GetAug21()).Return(new DateTime(2022, 8, 20));
+
+			sut.Execute().Day.Should().Be(20);
+		}
+
+		[Fact]
+		public void When_invalid_state_machine_attribute_Should_skip()
+		{
+			var fake = new Fake<TestClass>();
+
+			var sut = fake.Rewrite(f => f.GetDateWithInvalidMachine());
+			sut.Replace((StateMachine m) => m.GetAug21()).Return(new DateTime(2022, 8, 20));
+
+			sut.Execute().Day.Should().Be(21);
+		}
+
+		[Fact]
+		public void When_invalid_attribute_Should_skip()
+		{
+			var fake = new Fake<TestClass>();
+
+			var sut = fake.Rewrite(f => f.GetDateWithInvalidMachine());
+			sut.Replace((StateMachine m) => m.GetAug21()).Return(new DateTime(2022, 8, 20));
+
+			sut.Execute().Day.Should().Be(21);
+		}
+
 		private class AmbiguousCtorTestClass
 		{
 			private readonly object _arg;
@@ -146,6 +179,53 @@ namespace AutoFake.FunctionalTests
 				}
 				return i;
 			}
+
+			[System.Runtime.CompilerServices.AsyncStateMachine(typeof(StateMachine))]
+			public DateTime GetDateWithValidMachine()
+			{
+				var stateMachine = new StateMachine();
+				stateMachine.CallMoveNext();
+				return stateMachine.Date;
+			}
+
+			[StateMachine(typeof(StateMachine))]
+			public DateTime GetDateWithInvalidMachine()
+			{
+				var stateMachine = new StateMachine();
+				stateMachine.CallMoveNext();
+				return stateMachine.Date;
+			}
+
+			[StateMachine(typeof(Attribute))]
+			public DateTime GetDateWithoutdMachine()
+			{
+				var stateMachine = new StateMachine();
+				stateMachine.CallMoveNext();
+				return stateMachine.Date;
+			}
+		}
+
+		private class StateMachineAttribute : Attribute
+		{
+			public StateMachineAttribute(Type type) { }
+		}
+
+		private class StateMachine
+		{
+			public DateTime Date { get; private set; }
+
+			public void CallMoveNext()
+			{
+				var method = GetType().GetMethod(nameof(MoveNext));
+				method.Invoke(this, null);
+			}
+
+			public void MoveNext()
+			{
+				Date = GetAug21();
+			}
+
+			public DateTime GetAug21() => new DateTime(2022, 8, 21);
 		}
 	}
 }
