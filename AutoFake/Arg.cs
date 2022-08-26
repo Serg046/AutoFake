31866301627bf93ceb9a55d669df45c70ead99bf
Serg046 <serg046@outlook.com>
2022-08-26 23:36:03 +0300

    Remove some [ExcludeFromCodeCoverage] from Arg

--- AutoFake/Arg.cs
+++ AutoFake/Arg.cs
@@ -26,12 +26,10 @@ namespace AutoFake
 		[ExcludeFromCodeCoverage]
 		public static T Is<T>(Func<T, bool> checkArgumentFunc) => IsAny<T>();
 
-		[ExcludeFromCodeCoverage]
 		public static T Is<T>(T argument, IFakeArgumentChecker.Comparer<T> comparer) => IsAny<T>();
 
 		public static T IsAny<T>() => default!;
 
-		[ExcludeFromCodeCoverage]
 		internal class TypeWrapper
 		{
 			public TypeWrapper(Type type) => Type = type;
